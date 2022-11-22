93b2f5aa891caf0b96b99b665d170f1ece9b7d41
NazarbekAld <nazaralda74@gmail.com>
2022-11-22 11:21:52 -0800

    block.py filled.

--- blockchain/block.py
+++ blockchain/block.py
@@ -5,5 +5,19 @@ from Crypto.Hash import SHA256
 # Block
 
 class Block():
-    def __init__(self, transactions : list[transaction.Transaction], lastblockhash=SHA256.new()) -> None:
+    def __init__(self, transactions : list[transaction.Transaction], lastblockhash=SHA256.new().hexdigest(), lastblockindex=0) -> None:
+        self.__transactions = transactions
+        self.__myhash = SHA.new(data=lastblockhash.encode())
+        self.__index = lastblockindex
+    def getValues(self) -> list:
+        return self.__transactions, self.__myhash
+    def toJSON(self) -> str:
+        a = []
+        for i in self.__transactions:
+            a.append(json.dumps(i.getTransaction(returns="dict")))
+        json.dumps({
+            "index": self.__index,
+            "Transactions" : a,
+            "hash": self.__myhash
+            })
         pass
--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -16,10 +16,18 @@ class Transaction():
         self.__payee = payee
         self.__amount = amount
         self.__signature = signature
-    def getTransaction(self) -> HashMap:
-        map = HashMap()
-        map.put("payer", self.__payer)
-        map.put("payee", self.__payee)
-        map.put("amount", self.__amount)
-        map.put("signature", self.__signature)
-        return map
+    def getTransaction(self, returns="hashmap") -> HashMap or dict:
+        if returns == "hashmap":
+            map = HashMap()
+            map.put("payer", self.__payer)
+            map.put("payee", self.__payee)
+            map.put("amount", self.__amount)
+            map.put("signature", self.__signature)
+            return map
+        elif returns == "dict":
+            return {
+                "Payer": self.__payer.getAddress().export_key(),
+                "Payee": self.__payee.getAddress().export_key(),
+                "Amount": self.__amount,
+                "Signature": self.__signature.get().hexdigest()
+                }
